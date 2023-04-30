9baa2d97182cfb5ac364a2c6241b5cca6f76192f
Gabriel <prismaticnetwork12@gmail.com>
2023-04-30 18:41:12 -0300

    feat: resourcesconfiguration implemented

--- src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+  @Value("${jwt.secret}")
+  private String jwtSecret;
   //Componente do spring igual ao service, só que serve para metodo
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
@@ -16,7 +19,7 @@ public class AppConfig {
   @Bean
   public JwtAccessTokenConverter accessTokenConverter() {
     JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-    tokenConverter.setSigningKey("MY-JWT-SECRET");
+    tokenConverter.setSigningKey(jwtSecret);
     return tokenConverter;
   }
 
